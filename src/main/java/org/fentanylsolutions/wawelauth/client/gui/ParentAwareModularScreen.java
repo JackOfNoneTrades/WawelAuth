b319e2058277aa1c65decd7f56ca9d7a34985598
jack <181404551+JackOfNoneTrades@users.noreply.github.com>
2026-06-06 13:36:33 +0200

    Fixed escape key not working in mui guis

--- src/main/java/org/fentanylsolutions/wawelauth/client/gui/ParentAwareModularScreen.java
+++ src/main/java/org/fentanylsolutions/wawelauth/client/gui/ParentAwareModularScreen.java
@@ -3,11 +3,17 @@ package org.fentanylsolutions.wawelauth.client.gui;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
 
+import org.fentanylsolutions.fentlib.util.drop.GuiTransitionScheduler;
+import org.lwjgl.input.Keyboard;
+
 import com.cleanroommc.modularui.api.IMuiScreen;
 import com.cleanroommc.modularui.screen.CustomModularScreen;
+import com.cleanroommc.modularui.screen.ModularPanel;
 
+import cpw.mods.fml.common.Optional;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import me.eigenraven.lwjgl3ify.api.InputEvents;
 
 /**
  * Keeps the parent GUI "live" while a ModularUI screen is open.
@@ -15,6 +21,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 @SideOnly(Side.CLIENT)
 public abstract class ParentAwareModularScreen extends CustomModularScreen {
 
+    private static final String LWJGL3IFY_MOD_ID = "lwjgl3ify";
     private static final int OFFSCREEN_MOUSE_X = -10_000;
     private static final int OFFSCREEN_MOUSE_Y = -10_000;
 
@@ -48,6 +55,27 @@ public abstract class ParentAwareModularScreen extends CustomModularScreen {
         super.drawScreen();
     }
 
+    @Override
+    public boolean onKeyPressed(char typedChar, int keyCode) {
+        if (super.onKeyPressed(typedChar, keyCode)) {
+            return true;
+        }
+
+        return closeFromEscape(keyCode);
+    }
+
+    @Optional.Method(modid = LWJGL3IFY_MOD_ID)
+    @Override
+    public void onKeyEvent(InputEvents.KeyEvent event) {
+        if (dispatchLwjgl3ifyKeyEvent(event)) {
+            return;
+        }
+
+        if (event.action == InputEvents.KeyAction.PRESSED) {
+            scheduleCloseFromEscape(event.lwjgl2KeyCode);
+        }
+    }
+
     private void drawParentScreen() {
         Minecraft mc = Minecraft.getMinecraft();
         GuiScreen parent = getContext().getParentScreen();
@@ -58,5 +86,40 @@ public abstract class ParentAwareModularScreen extends CustomModularScreen {
         // Render the parent as a dynamic backdrop, but with an off-screen cursor so
         // hover state, tooltips, and any mouse-driven visuals are suppressed.
         parent.drawScreen(OFFSCREEN_MOUSE_X, OFFSCREEN_MOUSE_Y, getContext().getPartialTicks());
+    }
+
+    @Optional.Method(modid = LWJGL3IFY_MOD_ID)
+    private boolean dispatchLwjgl3ifyKeyEvent(InputEvents.KeyEvent event) {
+        for (ModularPanel panel : getPanelManager().getOpenPanels()) {
+            if (panel.onKeyEvent(event)) {
+                return true;
+            }
+            if (panel.disablePanelsBelow()) {
+                break;
+            }
+        }
+        return false;
+    }
+
+    private boolean closeFromEscape(int keyCode) {
+        Minecraft mc = Minecraft.getMinecraft();
+        if (keyCode != Keyboard.KEY_ESCAPE || mc.theWorld != null || !isActive()) {
+            return false;
+        }
+
+        if (getContext().hasDraggable()) {
+            getContext().dropDraggable(true);
+        } else {
+            getPanelManager().closeTopPanel();
+        }
+        return true;
+    }
+
+    @Optional.Method(modid = LWJGL3IFY_MOD_ID)
+    private void scheduleCloseFromEscape(int keyCode) {
+        if (keyCode != Keyboard.KEY_ESCAPE) {
+            return;
+        }
+        GuiTransitionScheduler.nextTick(() -> closeFromEscape(keyCode));
     }
 }
