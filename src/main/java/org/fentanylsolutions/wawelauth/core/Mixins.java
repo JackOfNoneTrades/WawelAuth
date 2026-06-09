6f8422da72a121f2c86d7682bef33ac4d7d8f393
jack <181404551+JackOfNoneTrades@users.noreply.github.com>
2026-06-09 08:35:53 +0200

    Moved lotr patch to fentlib

--- src/main/java/org/fentanylsolutions/wawelauth/core/Mixins.java
+++ src/main/java/org/fentanylsolutions/wawelauth/core/Mixins.java
@@ -222,12 +222,6 @@ public class Mixins extends FentMixins {
             .side(MiscUtil.Side.CLIENT)
             .build();
 
-        registry.mixin("MixinLOTRGuiMap")
-            .modid("lotr")
-            .phase(MixinUtil.Phase.LATE)
-            .side(MiscUtil.Side.CLIENT)
-            .build();
-
         registry.mixin("MixinVoiceHandler")
             .modid("ServerTools")
             .phase(MixinUtil.Phase.LATE)
