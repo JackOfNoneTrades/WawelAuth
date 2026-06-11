d1d367b7afe3d53bf83fb0f23d6837c8ce779025
jack <181404551+JackOfNoneTrades@users.noreply.github.com>
2026-06-11 11:41:40 +0200

    Pin 67 publishing script version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,4 @@ plugins {
     id("com.gtnewhorizons.gtnhconvention")
 }
 
-apply(from = "https://raw.githubusercontent.com/JackOfNoneTrades/67minecraft-gradle-publish/master/67minecraft-publish.gradle.kts")
+apply(from = "https://raw.githubusercontent.com/JackOfNoneTrades/67minecraft-gradle-publish/${property("publish67ScriptTag")}/67minecraft-publish.gradle.kts")
